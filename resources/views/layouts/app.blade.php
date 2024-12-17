f1ede820e45729c1510d13c133cc2f240705f66e
killflex <ferryhasanbackup@gmail.com>
2024-12-17 15:12:59 +0700

    fix: remove active nav class

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -107,7 +107,7 @@
               </button>
               <div class="collapse navbar-collapse" id="ftco-nav">
                 <ul class="navbar-nav ml-auto">
-                  <li class="nav-item active"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
+                  <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
                   <li class="nav-item"><a href="{{ route('products.menu') }}" class="nav-link">Menu</a></li>
                   <li class="nav-item"><a href="{{ route('services') }}" class="nav-link">Services</a></li>
                   <li class="nav-item"><a href="{{ route('about') }}" class="nav-link">About</a></li>
